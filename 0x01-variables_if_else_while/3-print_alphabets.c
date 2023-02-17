c28704b49120e639ede38441946cb7a11c658de4
Lotolim Philip Pkiach <philippkiach686@gmail.com>
2023-02-17 03:42:23 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,22 +7,17 @@
  */
 int main(void)
 	char c = 'a';
-
 	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
-
 	c = 'A';
-
 	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
-
 	}
-
 	putchar('\n');
 	return (0);
 }
