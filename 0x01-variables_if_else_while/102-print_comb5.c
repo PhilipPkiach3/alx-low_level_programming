47b7961071ab1ab2f40bc5d3c9f48f90c2167fa7
Lotolim Philip Pkiach <philippkiach686@gmail.com>
2023-02-19 11:25:46 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 /**
  * main - main black
- * Descrption:Write a program that prints all possible combinations of two two-digit numbers
+ * Descrption: Write a program that prints all possible combinations of two two-digit numbers
  */
 int main(void)
 {
     int i, j;
-    
     for (i = 0; i < 10; i++)
     {
-        for (j = 0; j < 10; j++)
+     	for (j = 0; j < 10; j++)
         {
             putchar(i + '0');
             putchar(j + '0');
             putchar(' ');
             putchar(j + '0');
             putchar(i + '0');
-            
-	    if (i != 9 || j != 9)
+            if (i != 9 || j != 9)
             {
                 putchar(',');
                 putchar(' ');
             }
         }
     }
-    return 0;
+    return (0);
 }
