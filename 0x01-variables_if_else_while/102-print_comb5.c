5fd47e99be09a4c7ffeeb9b2038f8d5fe084b276
Lotolim Philip Pkiach <philippkiach686@gmail.com>
2023-02-19 11:19:22 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,6 +6,7 @@
 int main(void)
 {
     int i, j;
+    
     for (i = 0; i < 10; i++)
     {
         for (j = 0; j < 10; j++)
@@ -15,7 +16,8 @@ int main(void)
             putchar(' ');
             putchar(j + '0');
             putchar(i + '0');
-            if (i != 9 || j != 9)
+            
+	    if (i != 9 || j != 9)
             {
                 putchar(',');
                 putchar(' ');
