e8140e7d2a1552b2b0a2dd55ef2250a2734f8b62
Lotolim Philip Pkiach <philippkiach686@gmail.com>
2023-02-21 08:03:29 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
 
 /**
- * write char c to stdout
- * Return: 1 on success
- * Return: -1 on error
+ * Write char c to stdout
+ *
+ * Return : 1 on success
+ * Return : -1 on error
  */
+
 int _putchar(char myChar)
 {
-        return (write(1, &myChar, 1));
+	return (write(1, &myChar, 1));
 }
