50b7fda7024ef77206a24a3c7d00d613ed9b7d9b
Lotolim Philip Pkiach <philippkiach686@gmail.com>
2023-02-21 05:48:38 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Return void
+ */
+void print_alphabet(void)
+{
+	char myStart;
+
+	for (myStart = 'a'; myStart <= 'z'; myStart++)
+	{
+		_putchar(myStart);
+	}
+	_putchar('\n');
+}
